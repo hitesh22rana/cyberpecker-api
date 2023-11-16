d9b2cc6d8a46e0cc0d74d79a2e3c9bb42ef45c63
hitesh <hitesh22rana@gmail.com>
2023-11-16 13:27:17 +0530

    chore: added echo server

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	cybernews "github.com/hitesh22rana/cyberpecker-api/pkg"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-func main() {
-	data, err := cybernews.GetNews("general")
+var port = ":8000"
 
+func getNews(c echo.Context) error {
+	newsType := c.QueryParam("type")
+	news, err := cybernews.GetNews(newsType)
 	if err != nil {
-		panic(err)
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
+	return c.JSON(http.StatusOK, news)
+}
 
-	fmt.Println(len(data))
+func main() {
+	e := echo.New()
+
+	api := e.Group("/api/v2", middleware.RemoveTrailingSlash())
+	api.GET("/news", getNews)
+
+	e.Logger.Fatal(e.Start(port))
 }
